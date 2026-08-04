af1bd7dd06c600db9dbe4cc41416da18613382d9
ujwalacherry89 <ujwalakamjula789@gmail.com>
2026-08-04 17:15:13 +0530

    division and floor division prg

---
+++ track/div_floor.py
@@ -0,0 +1,8 @@
+first_number = int(input()) 
+second_number = int(input()) 
+
+division_result = first_number / second_number 
+floor_division_result = first_number // second_number 
+
+print(division_result) 
+print(floor_division_result)
