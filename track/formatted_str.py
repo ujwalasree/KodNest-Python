750a5a83e4cc1098145b8e20e33ee6011cc60d5c
ujwalacherry89 <ujwalakamjula789@gmail.com>
2026-08-04 17:12:54 +0530

    formatted string prg

---
+++ track/formatted_str.py
@@ -0,0 +1,14 @@
+# Store the student details in the specified variable names
+student_name = input()
+student_age = int(input())
+course_rating = float(input())
+
+# Display the entered values
+print(f"Student: {student_name}")
+print(f"Age: {student_age}")
+print(f"Rating: {course_rating}")
+
+# Display the data types
+print(type(student_name))
+print(type(student_age))
+print(type(course_rating))
