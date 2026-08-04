c520e79b2e1c60987b3aef47cca238bcead85925
ujwalasree <ujwalakamjula789@gmail.com>
2026-08-04 17:22:30 +0530

    Delete track/div_floor.py
    Signed-off-by: ujwalasree <ujwalakamjula789@gmail.com>

--- track/div_floor.py
+++
@@ -1,8 +0,0 @@
-first_number = int(input()) 
-second_number = int(input()) 
-
-division_result = first_number / second_number 
-floor_division_result = first_number // second_number 
-
-print(division_result) 
-print(floor_division_result)
