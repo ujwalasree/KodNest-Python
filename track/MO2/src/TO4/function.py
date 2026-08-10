ef7bdd954d175442e7652f940ac3ec56050eb366
ujwalacherry89 <ujwalakamjula789@gmail.com>
2026-08-10 17:26:41 +0530

    Calculate a Invoice total using function prg

---
+++ track/MO2/src/TO4/function.py
@@ -0,0 +1,8 @@
+def display_invoice_total(price, quantity):
+    total = price * quantity
+    print(f"Total: {total}")
+
+price = int(input())
+quantity = int(input())
+
+display_invoice_total(price, quantity)
