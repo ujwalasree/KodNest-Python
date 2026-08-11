829d78f0bca849aa76976113d9ccdd28204ee1ce
ujwalacherry89 <ujwalakamjula789@gmail.com>
2026-08-11 16:53:47 +0530

    student details using different arguments prg

---
+++ track/MO2/src/TO4/argument_types.py
@@ -0,0 +1,6 @@
+def display_student(name, course, level):
+    print(f"{name} | {course} | {level}")
+
+display_student("Aarav", "Python", "Beginner")
+display_student(name="Meera", course="Java", level="Intermediate")
+display_student("Kabir", course="SQL", level="Beginner")
